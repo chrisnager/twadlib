1971d60346d112cab7943bd665221ada7630f128
Tom Miller <jackerran@gmail.com>
2011-09-23 17:43:14 -0500

    Add license and change error

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2011 Christopher Nager
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
 var regexs=[/NAME/g,/PLURAL_NOUN/g,/NOUN/g,/ADJECTIVE/g,/PRESENT_VERB/g];
 var stories=[ "<h1>AUTHOR_NAME says:</h1><p>I sure am tired of these ADJECTIVE PLURAL_NOUN on this ADJECTIVE NOUN.</p>",
 "<h1>AUTHOR_NAME says:</h1><p>If it was ADJECTIVE to PRESENT_VERB NOUN, everyone would do it.</p>",
@@ -27,6 +50,6 @@ $.ajax({
       }
     }
   },
-  error : function() { alert('Fail Whale!'); },
+  //  error : function() { alert('Fail Whale!'); },
 });
 $(function() { $('#logo').click(function() { location.reload(); }); });
